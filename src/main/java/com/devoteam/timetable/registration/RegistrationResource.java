dffd3bd1a42b729e76d5e1832fb1972daed5f4f0
Jens Bus <hijneke14@gmail.com>
2018-09-21 09:48:27 +0200

    Exceptionhandler makes error clearer

--- src/main/java/com/devoteam/timetable/registration/RegistrationResource.java
+++ src/main/java/com/devoteam/timetable/registration/RegistrationResource.java
@@ -3,14 +3,22 @@ package com.devoteam.timetable.registration;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
